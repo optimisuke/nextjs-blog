923865a06b904f7d6f6a9a6ac1d4f46cca349fe1
Optimisuke <naosuke0219@gmail.com>
2022-10-23 21:54:13 +0900

    get data from microcms

---
+++ out/_next/static/4rBqVrRdueLtFPvQKnf4M/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-61b2b7c17410c03c.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],"/blog/[id]":["static/chunks/pages/blog/[id]-5888ce71ba6aa532.js"],sortedPages:["/","/_app","/_error","/blog/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
